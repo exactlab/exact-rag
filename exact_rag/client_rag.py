88abcd7b60f9cbbd5f3dcbbbfdd0c2c0b9d42fc6
mfranzon <franzonmarco94@gmail.com>
2024-01-11 21:49:28 +0100

    add pre-commit

--- exact_rag/client_rag.py
+++ exact_rag/client_rag.py
@@ -1,34 +1,40 @@
 import requests
+
 from rich import print
 
 
 BASE_URL = "http://0.0.0.0:8080"
+
 
 def upload_file(file_path, endpoint="/upload_text/"):
     with open(file_path, "rb") as file:
         files = {"file": file}
         response = requests.post(f"{BASE_URL}{endpoint}", files=files)
         return response.json()
-    
+
+
 def upload_audio(file_path, endpoint="/upload_audio/"):
-    files = {'audio': (f'{file_path}', open(f'{file_path}', 'rb'), 'audio/mp3')}
+    files = {"audio": (f"{file_path}", open(f"{file_path}", "rb"), "audio/mp3")}
     response = requests.post(f"{BASE_URL}{endpoint}", files=files)
     return response.json()
 
+
 def upload_image(file_path, endpoint="/upload_image/"):
-    files = {'image': (file_path, open(file_path, 'rb'), 'image/png')}
+    files = {"image": (file_path, open(file_path, "rb"), "image/png")}
     response = requests.post(f"{BASE_URL}{endpoint}", files=files)
     return response.json()
+
 
 def send_query(query, endpoint="/query/"):
     data = {"text": query}
     response = requests.post(f"{BASE_URL}{endpoint}", json=data)
     return response.json()
 
+
 if __name__ == "__main__":
     # Example: Upload a file
     # file_response = upload_file("test.txt")
-    
+
     # file_response = upload_audio("test.mp3")
 
     # file_response = upload_image("test.png")
