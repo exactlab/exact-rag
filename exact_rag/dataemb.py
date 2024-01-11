e21487b7e3bba01747c947e4761702f22ff1d853
Matteo Poggi <matteo.poggi.fi@gmail.com>
2024-01-11 19:18:21 +0100

    Add Caller class to handle multiple variants.

--- exact_rag/dataemb.py
+++ exact_rag/dataemb.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Callable
 
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.embeddings import OllamaEmbeddings
@@ -14,62 +14,54 @@ from langchain_openai import ChatOpenAI
 from langchain_community.chat_models import ChatOllama
 
 
+class Caller:
+    def __init__(self, callable: Callable[..., Any], arg_swap: dict[str, Any] | None = None, accept_only: list[str] | None = None):
+        self._callable = callable
+        self._arg_swap = arg_swap
+        self._accept_only = accept_only
+
+    def __call__(self, **args):
+        if self._accept_only:
+            args = {key: value for (key, value) in args.items() if key in self._accept_only}
+
+        if self._arg_swap:
+            args = {self._arg_swap.get(arg, arg): value for (arg, value) in args.items()}
+
+        return self._callable(**args)
+
+
+embeddings = {"openai": Caller(OpenAIEmbeddings, accept_only=["api_key"]),
+              "ollama": Caller(OllamaEmbeddings, accept_only=["model"])}
+
+dbs = {"chroma": Caller(Chroma, {"embedding": "embedding_function"}, accept_only=["embedding", "persist_directory", "collection_name"]),
+       "elastic": Caller(ElasticsearchStore, {"collection_name": "index_name", "url": "es_url"} ,accept_only=["embedding", "url", "collection_name", "distance_strategy", "strategy"])}
+
+chats = {"openai": Caller(ChatOpenAI, {"api_key": "openai_api_key"}, accept_only=["model", "temperature", "api_key"]),
+         "ollama": Caller(ChatOllama, accept_only=["model", "temperature"])}
+
+
 class DataEmbedding:
     def __init__(self, settings: dict[str, Any]):
         embedding_settings = settings["embedding"]
         embedding_type = embedding_settings["type"]
-        if embedding_type == "openai":
-            self._embedding = OpenAIEmbeddings(api_key=embedding_settings["api_key"])
-
-        elif embedding_type == "ollama":
-            self._embedding = OllamaEmbeddings(model=embedding_settings["model"])
-
-        else:
-            print(f"Embedding {embedding_type} not supported.")
-            return
+        self._embedding = embeddings[embedding_type](**embedding_settings)
 
         database_settings = settings["database"]
         database_type = database_settings["type"]
-        if database_type == "chroma":
-            self._vectorstore = Chroma(embedding_function=self._embedding,
-                                       persist_directory=database_settings["persist_directory"],
-                                       collection_name=database_settings["collection_name"])
-        
-        elif database_type == "elastic":
-            self._vectorstore = ElasticsearchStore(embedding=self._embedding,
-                                                   es_url=database_settings["url"],
-                                                   index_name=database_settings["collection_name"],
-                                                   distance_strategy=database_settings["distance_strategy"],
-                                                   strategy=ElasticsearchStore.ExactRetrievalStrategy())
 
-        else:
-            print(f"Database {database_type} not supported.")
-            return
+        self._vectorstore = dbs[database_type](embedding=self._embedding, **database_settings, strategy=ElasticsearchStore.ExactRetrievalStrategy())
 
         self._record_manager = SQLRecordManager(database_settings["sql"]["namespace"],
                                                 db_url=database_settings["sql"]["url"])
         self._record_manager.create_schema()
         self._splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=database_settings["splitter"]["chunk_size"],
-                                                                                chunk_overlap=database_settings["splitter"]["chunk_overlap"])
+                                                                              chunk_overlap=database_settings["splitter"]["chunk_overlap"])
 
-        if embedding_type == "openai":
-            self._qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model=embedding_settings["chat"]["model_name"],
-                                                        temperature=embedding_settings["chat"]["temperature"],
-                                                        openai_api_key=embedding_settings["api_key"]),
-                                        chain_type=embedding_settings["chain_type"],
-                                        retriever=self._vectorstore.as_retriever(search_type=embedding_settings["search"]["type"],
-                                                                                 search_kwargs={'k': embedding_settings["search"]["k"],
-                                                                                                'fetch_k': embedding_settings["search"]["fetch_k"]}))
-
-        elif embedding_type == "ollama":
-            self._qa = RetrievalQA.from_chain_type(llm=ChatOllama(model=embedding_settings["chat"]["model_name"],
-                                                                  temperature=embedding_settings["chat"]["temperature"]),
-                                                   chain_type=embedding_settings["chain_type"],
-                                                   retriever=self._vectorstore.as_retriever(search_type=embedding_settings["search"]["type"],
-                                                                                            search_kwargs={'k': embedding_settings["search"]["k"],
-                                                                                                           'fetch_k': embedding_settings["search"]["fetch_k"]}))
-
-
+        self._qa = RetrievalQA.from_chain_type(llm=chats[embedding_type](**embedding_settings["chat"], **embedding_settings),
+                                               chain_type="stuff",
+                                               retriever=self._vectorstore.as_retriever(search_type=embedding_settings["search"]["type"],
+                                                                                        search_kwargs={'k': embedding_settings["search"]["k"],
+                                                                                                       'fetch_k': embedding_settings["search"]["fetch_k"]}))
 
     def load(self, text: str):
         id_key = "hash"
